2a07e0f2757e5146ae5330dcd48b19a5c6fba3bd
Cássia Maués Cuóco <ccmaues@gmail.com>
2023-08-04 16:49:06 -0300

    New package for argument usage

--- 6_auto_scrips/stat_description.R
+++ 6_auto_scrips/stat_description.R
@@ -1,43 +1,48 @@
 #!/usr/bin/env Rscript
+library(docopt)
+library(glue)
+doc <- "
+Usage:
+  my_script.R [--input FILE] [--tool TOOL] [--output FILE] [--var-path FILE]
+
+Options:
+  -i, --input FILE         # PRS file with scores [ file.profile | file.all_score ]
+  -t, --tool TOOL          # Type of tool used [ PRSice2 | PRSCS ]
+  -o, --output FILE        # Output file names
+  -p, --var-path FILE      # RDS file with variables [ file.RDS ]
+  -v, --vars               # Column names to be used in file.RDS [ var1,var2,var3 ] 
+"
+
+args <- docopt(doc)
+input <- args[["--input"]]
+tool <- args[["--tool"]]
+output <- args[["--output"]]
+var_path <- args[["--var-path"]]
+vars <- args[["--vars"]]
+
+vars <- unlist(strsplit(vars, ","))
+
 # message("
 # ------------------------------
 # Loading necessary packages...
 # ------------------------------"
 # )
-# might have problems
 # pacman::p_load(
 #     psych, dplyr, glue, ggplot2,
 #     tidyr, patchwork, optparser
 #     ) # add ggthemr later
-library(argparse)
-library(glue)
-parser <- ArgumentParser(
-    description = "Script for statistical description of a variable"
-    )
-parser$add_argument(
-    "--input",
-    dest = "input_file",
-    help = "INPUT file name with or without path [MUST BE RDS FORMAT]")
-parser$add_argument(
-    "--output",
-    dest = "opt_file",
-    default = "output_stat",
-    help = "OUTPUT file name [MULTIPLE OUTPUT FORMATS]"
-    )
-parser$add_argument(
-    "--output-dir",
-    dest = "opt_path",
-    help = "OUTPUT path"
-)
 
-args <- parser$parse_args()
-input_file <- args$input_file
-opt_file <- args$opt_file
-opt_dir <- args$opt_path
+message(glue("This is the {input} file"))
+message(glue("This is the {tool} file"))
+message(glue("This is the {output} file"))
+message(glue("This is the {var_path} path"))
+message(glue("This is the {vars} path"))
 
-message(glue("This is the {input_file} file"))
-message(glue("This is the {opt_file} file"))
-message(glue("This is the {opt_dir} path"))
-
-#vars <- readRDS(glue("{obj_dir}/{input_file}.RDS"))
+#variables <- readRDS(glue("{var}"))
 #ggthemr("fresh")
+
+## Count N in var
+
+## Test normality
+
+## Make plots
