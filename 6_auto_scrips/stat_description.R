53aaeac2b125a7b33e29fe8c64ad5f3e659a4b12
ccmaues <ccmaues@gmail.com>
2023-08-15 17:50:24 -0300

    Fixed major error for numeric variables

--- 6_auto_scrips/stat_description.R
+++ 6_auto_scrips/stat_description.R
@@ -112,30 +112,21 @@ message("
 #### Making normality test for variables . . .
 ")
 if (ncol(num_test) != 0) {
-    for (i in 1:ncol(for_use)) {
-      if (names(for_use)[i] != "PRS") {
-        col_name <- names(for_use)[i]
-        fcolumn <- levels(for_use[[i]])
-        for_sep <- data.frame(for_use[[i]], for_use[["PRS"]])
-        colnames(for_sep) <- c(col_name, "PRS")
-        for (level in fcolumn) {
-          subset_data <- for_sep %>%
-            filter(!!sym(col_name) == level)
-          new_df_name <- paste(col_name, level, sep = "_")
-          subset_dfs[[new_df_name]] <- subset_data
-        }
-      }
-    }
+  for (i in 1:ncol(for_use)) {
+    for_sep <- data.frame(for_use[[i]])
+    col_name <- colnames(for_use)[i]
+    colnames(for_sep) <- col_name
+    subset_dfs[[col_name]] <- for_sep
     subset_names <- names(subset_dfs)
-
     for (name in subset_names) {
       data <- subset_dfs[[name]]
-      d <- psych::describe(data$PRS)
-      t <- nortest::ad.test(data$PRS)
+      d <- psych::describe(data[[glue("{name}")]])
+      t <- nortest::ad.test(data[[glue("{name}")]])
 
       description[[name]] <- make_describe_df(d)
       test[[name]] <- make_test_df(t)
-    }
+     }
+  }
 } else {
   norm_test <- select(variables, IID, all_of(vars)) %>%
     inner_join(., prs_values, by = "IID") %>%
