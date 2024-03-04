1db9a758d7ec827c29496483a996df9ab1d8f763
Cássia Maués Cuóco <67009042+ccmaues@users.noreply.github.com>
2024-03-04 15:45:18 -0300

    Update functions_to_source.R

--- functions_to_source.R
+++ functions_to_source.R
@@ -1,6 +1,30 @@
 # Functions for source in other codes
+# and datas that are standard in the analysis
 # less "polution" in main codes
 # created for use in our data
+
+pacman::p_load(
+  glue, dplyr, ggplot2, remotes, tidyr,
+  envalysis, extrafont, glue, ggthemr, data.table
+)
+
+#font_import()
+loadfonts()
+
+# check system
+if (Sys.info()["sysname"] == "Linux") {
+  Path <- "/media/santorolab/C207-3566"
+  font <- "Ubuntu Condensed"
+} else {
+  Path <- "D:"
+  font <- "Arial Narrow"
+}
+pheno <- readRDS(glue("{Path}/objects_R/cass_BHRC_Mod_All_Phenotypes_29-02-2024.RDS"))
+ages <-	readRDS(glue("{Path}/objects_R/cass_BHRC_Age_Imputed_26-02-2024.RDS")) %>%
+  rename(age_W0 = W0, age_W1 = W1, age_W2 = W2)
+state <- readRDS(glue("{Path}/objects_R/cass_BHRC_STATE.RDS"))
+sex <- readRDS(glue("{Path}/objects_R/cass_BHRC_sex.RDS"))
+pc <- readRDS(glue("{Path}/objects_R/cass_BHRC_PC20.RDS"))
 
 # PRS value adjustment
 adjust_model <- function(data_list) {
