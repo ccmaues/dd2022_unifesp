bcad85c83c55356b114a36cb2e86ada163a3e09f
Cássia Maués Cuóco <67009042+ccmaues@users.noreply.github.com>
2024-04-22 17:18:03 -0300

    Update 17-04-2024_cass_BHRC_parents_phenotype.R

--- 17-04-2024_cass_BHRC_parents_phenotype.R
+++ 17-04-2024_cass_BHRC_parents_phenotype.R
@@ -2,8 +2,12 @@ source("functions_to_source.R")
 
 # dados fenotípicos dos pais associados com os IIDs
 parents <-
-readRDS(glue("{Path}/0_external_files/MINIeFLI.rds")) %>%
-select(FID, IID, starts_with("iMINI"), respondent)
+readRDS(glue("{Path}/0_external_files/Lucas_MINI_BHRCS.rds")) %>%
+select(FID, IID, starts_with("iMINI"), respondent) %>%
+mutate(across(3:23, as.numeric),
+across(3:23, ~case_when(
+  .x == 2 ~ 2,
+  .x == 1 ~ 0)))
 str(parents)
 
 table(parents$respondent)
@@ -19,26 +23,30 @@ select(var, type)
 # Juntar colunas e fazer novo resultado de fenótipo
 BP <- select(parents, IID, iMINI_cur_man, iMINI_lif_man, iMINI_cur_any_mood)
 MDD <- select(parents, IID, iMINI_cur_dep, iMINI_cur_rdep)
+
 ANX <-
 select(parents, IID, iMINI_lif_panic,
 iMINI_cur_panic, iMINI_cur_agor,
 iMINI_cur_social, iMINI_cur_gad,
 iMINI_cur_any_anx)
+
 DEP <-
 select(parents, IID, iMINI_cur_alcdep,
 iMINI_cur_alcabus, iMINI_cur_drugdep,
 iMINI_cur_drugabus, iMINI_cur_any_subs)
+
 SCZ <- select(parents, IID, iMINI_lif_psych, iMINI_cur_psych)
 ADHD <- select(parents, IID, iMINI_child_adhd, iMINI_cur_adhd)
 
 make_new_pheno <-
 function(data) {
-  new_col <- rowSums(data[, -1])
+  new_col <- rowSums(data[, c(-1)])
   case_when(
-  new_col != 0 & !is.na(new_col) ~ 1,
-  new_col == 0 ~ 0,
-  TRUE ~ NA)
+    new_col != 0 & !is.na(new_col) ~ 1,
+    new_col == 0 ~ 0,
+    TRUE ~ NA)
 }
+
 new_pheno <-
 data.frame(BP$IID, BP = make_new_pheno(BP),
 MDD = make_new_pheno(MDD), ANX = make_new_pheno(ANX),
